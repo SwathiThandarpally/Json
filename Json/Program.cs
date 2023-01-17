9f045e1786c110cfdf6937e7a0544632ec601464
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-17 15:04:41 +0530

    [Swathi]:Added Json Inventory Program

---
+++ Json/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Json
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Json Inventory Program");
+        }
+    }
+}
