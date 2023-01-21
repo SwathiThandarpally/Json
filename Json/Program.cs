b44ac5485b98592d1a79c92a2a520724623bcb2f
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-21 10:32:25 +0530

    Added Json Inventory

--- Json/Program.cs
+++ Json/Program.cs
@@ -20,9 +20,27 @@ namespace Json
                     case 1:
                         inventoryData.DisplayInventory(INVENTORY_DATA_PATH);
                         break;
-                    default:
+                    case 2:
+                        Console.WriteLine("Enter the Inventory List and Name :");
+                        string inventoryName1 = Console.ReadLine();
+                        string grain1 = Console.ReadLine();
+                        
+                        data.Weight = Convert.ToInt32(Console.ReadLine());
+                        data.PricePerKg = Convert.ToInt32(Console.ReadLine());
+                        manager.AddInventory(data, inventoryName1, INVENTORY_MANAGEMENT_PATH);
                         break;
-
+                    case 3:
+                        Console.WriteLine("Enter the Inventory List and Grains :");
+                        string inventoryName2 = Console.ReadLine();
+                        string grain2 = Console.ReadLine();
+                        manager.EditInventory(inventoryName2, grain2, INVENTORY_MANAGEMENT_PATH);
+                        break;
+                    case 4:
+                        check = false;
+                        break;
+                    default:
+                        Console.WriteLine("You chose the wrong option, please choose correctly");
+                        break;
                 }
             }
 
